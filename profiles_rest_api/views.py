35aa15f304219feaf16aaa61bb851469145ac24d
Ricardo B. Schalch <ricardo.schalch@gmail.com>
2017-12-25 21:06:32 -0200

    add search capabilities

--- profiles_rest_api/views.py
+++ profiles_rest_api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, filters
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -112,3 +112,5 @@ class UserProfileViewSet(ModelViewSet):
     queryset = UserProfile.objects.all()
     authentication_classes = (TokenAuthentication, )
     permission_classes = (UpdateOwnProfile, )
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
